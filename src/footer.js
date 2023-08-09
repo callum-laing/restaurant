dac24a8ba90e6bae8dcb7244ada4a935dc34b604
Callum Laing <callum.laing91@hotmail.co.uk>
2023-08-09 23:21:46 +0100

    added styling

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,7 @@
 function footerFunc() {
   const footer = document.createElement("div");
   footer.classList.add("footer");
+  footer.innerHTML = `<p class="footer-text">Developed by Callum Laing</p>`;
 
   return footer;
 }
--- src/home.js
+++ src/home.js
@@ -5,6 +5,11 @@ function homePage() {
   const wrapper = document.createElement("div");
   wrapper.classList.add("main-page");
 
+  const bodyText = document.createElement("div");
+  bodyText.classList.add("intro");
+  bodyText.innerHTML = `<h1 class="intro-color">Welcome</h1> <br> <h3>The Pantry</h3> <hr> <p class="intro-text">Where every bite is a bakery delight!</p>`;
+
+  wrapper.appendChild(bodyText);
   return wrapper;
 }
 
