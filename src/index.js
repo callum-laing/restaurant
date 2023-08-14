c5f503d9fe817359e83b9462b71ab7829eaab629
Callum Laing <callum.laing91@hotmail.co.uk>
2023-08-14 22:01:08 +0100

    added changes for testing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import wrapper from "./home";
+import homeFunc from "./home";
 import headerFunc from "./header";
 import footerFunc from "./footer";
 import menuPage from "./menu";
+import contactPage from "./contact";
 
-function mainFunction() {
+// function mainFunction(content) {
+//   const container = document.querySelector("#content");
+
+//   container.appendChild(headerFunc);
+//   container.appendChild(homeFunc);
+//   container.appendChild(footerFunc);
+// }
+
+// mainFunction();
+
+function clearContent() {
+  const content = document.querySelector("#content");
+  content.innerHTML = "";
+}
+
+function loadInitialContent(content) {
   const container = document.querySelector("#content");
-
+  clearContent();
   container.appendChild(headerFunc);
-  container.appendChild(wrapper);
+  container.appendChild(content);
   container.appendChild(footerFunc);
 }
 
-mainFunction();
+document.addEventListener("DOMContentLoaded", () => {
+  loadInitialContent(wrapper);
+
+  const homeLink = document.querySelector("#homeLink");
+  homeLink.addEventListener("click", () => {
+    loadInitialContent(homeFunc);
+  });
+
+  const contactLink = document.querySelector("#contactLink");
+  contactLink.addEventListener("click", () => {
+    loadInitialContent(contactPage);
+  });
+
+  const menuLink = document.querySelector("#menuLink");
+  menuLink.addEventListener("click", () => {
+    loadInitialContent(menuPage);
+  });
+});
